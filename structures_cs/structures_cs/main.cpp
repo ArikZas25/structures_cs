d3ebd9bd4ea600bb04d1b8cb6b92823252939fdb
ArikZas25 <Arik2510@gmail.com>
2026-01-09 11:16:10 +0200

    1

---
+++ structures_cs/structures_cs/main.cpp
@@ -0,0 +1,73 @@
+#include <random>
+#include <iostream>
+#include <string>
+#include "HashTable_Chaining.h"
+#include "HashTable_OpenAddressing.h"
+#include <iomanip>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::to_string;
+using std::fixed;
+using std::setprecision;//to set the precision of the average length output
+
+int seed = 600012;
+std::mt19937 rng(seed);
+int n = 3000;
+
+int main()
+{
+    string tableType;
+    int m, n, seed;
+    if (!(cin >> tableType >> m >> n >> seed)) {
+        return 0;
+    }
+    bool valid = true;
+    if (!m || tableType.size() == 0) valid = false;
+    if (m <= 0 || n < 0) valid = false;
+    if (tableType == "OpenAddressing" && n > m) valid = false;
+    if (!valid) {
+        cout << "wrong input" << endl;
+        return 0;
+    }
+    std::mt19937 rng(seed);
+    if (tableType == "Chaining") {
+        HashTable_Chaining table;
+        table.Init(m);
+        cout << "Inserts:" << endl;
+        for (int i = 0; i < n; i++) {
+            int num = rng() % 1000000;
+            string data = "User" + to_string(num);
+            int res = table.Insert(num, data);
+            cout << res << endl;
+        }
+        cout << "Size: " << endl << table.size() << endl;
+        cout << "Max length: " << endl << table.Max_Length() << endl;
+        cout << "Average length:" << endl << fixed << setprecision(4) << table.Average_Length() << endl;
+        cout << "Find(100): " << endl << table.Find(100) << endl;
+    }
+    else if (tableType == "OpenAddressing") {
+        HashTable_OpenAddressing table;
+        table.Init(m);
+        cout << "Inserts:" << endl;
+        for (int i = 0; i < n; i++) {
+            int num = rng() % 1000000;
+            string data = "User" + to_string(num);
+            int res = table.Insert(num, data);
+            cout << res << endl;
+        }
+
+        cout << "Size: " << endl << table.size() << endl;
+        cout << "Max length: " << endl << table.Max_Length() << endl;
+        cout << "Average length:" << endl << fixed << setprecision(4) << table.Average_Length() << endl;
+        cout << "Find(100): " << endl << table.Find(100) << endl;
+    }
+    else {
+        cout << "wrong input" << endl;
+    }
+
+    return 0;
+
+}
